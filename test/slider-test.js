84225e069e200447d57d17771f17e5f755fc07d8
blackjk3 <blackjk@gmail.com>
2015-08-27 09:32:58 -0500

    Adding slider test. Will have to config later.

---
+++ test/slider-test.js
@@ -0,0 +1,29 @@
+// require('./dom-mock')('<html><body></body></html>');
+
+// var jsdom = require('mocha-jsdom');
+// var assert = require('assert');
+// var React = require('react/addons');
+// var TestUtils = React.addons.TestUtils;
+
+// import SliderNativeBootstrap from 'react-bootstrap-native-slider';
+
+// describe('Testing <Slider>', function() {
+
+//   //jsdom({ skipWindowCheck: true });
+
+//   it('should be able to set the default value', function() {
+//     var slider = TestUtils.renderIntoDocument(
+//       <SliderNativeBootstrap
+//         name='slider-test'
+//         value={2}
+//         step={1}
+//         max={5}
+//         min={0} />
+//     );
+//     // var element = React.findDOMNode(slider);
+//     console.log('hi');
+//     //var starDiv = element.querySelector('.rating-stars');
+    
+//     //assert.equal(starDiv.style.width, '50%');
+//   });
+// });
