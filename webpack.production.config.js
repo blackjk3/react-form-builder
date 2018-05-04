21e890621f2f0416bd5184a013dae75341762a05
Kiho <kihoc22@gmail.com>
2018-05-03 21:40:55 -0700

    Add script for umd build

---
+++ webpack.production.config.js
@@ -0,0 +1,64 @@
+var path = require('path');
+var webpack = require( 'webpack');
+
+module.exports = {
+  entry: './src/index.jsx',
+
+  output: {
+    path: path.resolve('./lib'),
+    filename: "app.js",
+    library: 'ReactFormBuilder',
+    libraryTarget: 'umd',
+  },
+
+  externals: {
+    //don't bundle the 'react' npm package with our bundle.js
+    //but get it from a global 'React' variable
+    'react': 'react',
+    'react-dom': 'react-dom',
+    'react-datepicker': 'react-datepicker',
+    'classnames': 'classnames',
+    'jquery': 'jquery',
+    'bootstrap': 'bootstrap'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.css', '.json']
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js|.jsx?$/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader', options: {
+              includePaths: ['./node_modules']
+            }
+          }
+        ]
+      },
+    ]
+  }
+};
