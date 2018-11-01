e1c3ff20bed29f19d48647e817bebba343d0cf5f
Kiho <kihoc@gmail.com>
2018-11-01 00:39:23 -0700

    Fix star rating style

--- src/star-rating.jsx
+++ src/star-rating.jsx
@@ -231,22 +231,23 @@ export default class StarRating extends React.Component {
           onMouseLeave={this.handleMouseLeave.bind(this)}
           onClick={this.handleClick.bind(this)}>
           <div className="rating-stars" data-content={this.state.glyph} style={{width: this.state.pos}}></div>
-          <input type="number" name={this.props.name} value={this.state.ratingCache.rating} style={{display: 'none !important'}} min={this.min} max={this.max} readOnly />
         </div>
       );
     } else {
       starRating = (
         <div ref={c => this.node = c} className="rating-container rating-gly-star" data-content={this.state.glyph}>
           <div className="rating-stars" data-content={this.state.glyph} style={{width: this.state.pos}}></div>
-          <input type="number" name={this.props.name} value={this.state.ratingCache.rating} style={{display: 'none !important'}} min={this.min} max={this.max} readOnly />
         </div>
       );
     }
 
     return (
       <span className="react-star-rating">
-        <span ref={c => this.rootNode = c} style={{cursor: 'pointer'}} className={classes}>
+        <span ref={c => this.rootNode = c} style={{cursor: 'pointer'}} className={classes}>        
           {starRating}
+          <input type="number" name={this.props.name} value={this.state.ratingCache.rating} style={{display: 'none !important'}} min={this.min} max={this.max} readOnly style={{
+            width: 65
+          }}/>
         </span>
       </span>
     );
