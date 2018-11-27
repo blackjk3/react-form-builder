2c31bf33224d2a7ed1bc9f6ce4e4cac957e06fbf
Kiho <kihoc@gmail.com>
2018-11-26 22:40:01 -0800

    Unregister clicklistener on unmount form-builder

--- src/index.jsx
+++ src/index.jsx
@@ -20,9 +20,14 @@ class ReactFormBuilder extends React.Component {
       editMode: false,
       editElement: null
     }
-    document.addEventListener("click", this.editModeOff.bind(this));
+    this.onDocumentClick = this.editModeOff.bind(this);
+    document.addEventListener("click", this.onDocumentClick, false);
   }
 
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onDocumentClick, false);
+  }
+  
   editModeOn(data, e) {
     e.preventDefault();
     e.stopPropagation();
@@ -43,15 +48,10 @@ class ReactFormBuilder extends React.Component {
   }
 
   editModeOff(e) {
-    //  const $menu = $(".edit-form");
-    //  let click_is_outside_menu = (!$menu.is(e.target) && $menu.has(e.target).length === 0);
     let click_is_outside_menu = !(e.target && e.target.closest(".edit-form")) && e.screenX > 0;
   
-    if (this.state.editMode && click_is_outside_menu) {
-      this.setState({
-        editMode: false,
-        editElement: null
-      });
+    if (click_is_outside_menu) {
+      this.manualEditModeOff();
     }
   }
 
