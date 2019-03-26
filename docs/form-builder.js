fc24e99137f0d2352d4e4f3a9d2710d23e532a78
Kiho <kihoc@gmail.com>
2019-03-25 20:28:37 -0700

    Delay loading - demo page

--- docs/form-builder.js
+++ docs/form-builder.js
@@ -19,4 +19,20 @@ if (!cid) {
 const url = `https://safe-springs-35306.herokuapp.com/api/formdata?cid=${cid}`;
 const saveUrl = `https://safe-springs-35306.herokuapp.com/api/formdata?cid=${cid}`;
 
-ReactDOM.render(e(FormBuilder, { url, saveUrl }), domContainer);
+function render() {
+  const element = document.querySelector('#loading');
+  element.style.display = 'none';
+  // Delay loading, wait until slow heroku back-end is waking up.
+  ReactDOM.render(e(FormBuilder, { url, saveUrl }), domContainer);
+}
+
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json; charset=utf-8',
+  OPTIONS: '',
+};
+
+fetch(url, {
+  method: 'GET',
+  headers,
+}).then(render);
