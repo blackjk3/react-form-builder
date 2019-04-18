fbb4b694905053f76869908a36b2f1ff05a91dfd
Kiho <kihoc@gmail.com>
2019-04-18 07:55:46 -0700

    Copy code from next to mongo

---
+++ examples/mongo/components/demobar.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import { ReactFormGenerator, ElementStore } from 'react-form-builder2';
+import { get, post } from '../components/requests';
+
+export default class Demobar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      previewVisible: false,
+      shortPreviewVisible: false,
+      roPreviewVisible: false,
+    };
+
+    this.submit = this.onSubmit.bind(this);
+    const update = this.onChange.bind(this);
+    ElementStore.subscribe(state => update(state.data));
+  }
+
+  showPreview() {
+    this.setState({
+      previewVisible: true,
+    });
+  }
+
+  showShortPreview() {
+    this.setState({
+      shortPreviewVisible: true,
+    });
+  }
+
+  showRoPreview() {
+    this.setState({
+      roPreviewVisible: true,
+    });
+  }
+
+  closePreview() {
+    this.setState({
+      previewVisible: false,
+      shortPreviewVisible: false,
+      roPreviewVisible: false,
+    });
+  }
+
+  onChange(data) {
+    this.setState({
+      data,
+    });
+  }
+
+  onSubmit(data) {
+    const { postUrl } = this.props;
+    console.log('onSubmit', data);
+    // Place code to post json data to server here
+    post(postUrl, data).then(x => {
+      window.location.href = '/form';
+    });
+    return false;
+  }
+
+  render() {
+    let modalClass = 'modal';
+    if (this.state.previewVisible) {
+      modalClass += ' show';
+    }
+
+    let shortModalClass = 'modal short-modal';
+    if (this.state.shortPreviewVisible) {
+      shortModalClass += ' show';
+    }
+
+    let roModalClass = 'modal ro-modal';
+    if (this.state.roPreviewVisible) {
+      roModalClass += ' show';
+    }
+
+    return (
+      <div className="clearfix" style={{ margin: '10px', width: '70%' }}>
+        <h4 className="pull-left">Preview</h4>
+        <button className="btn btn-primary pull-right" style={{ marginRight: '10px' }} onClick={this.showPreview.bind(this)}>Preview Form</button>
+        <button className="btn btn-default pull-right" style={{ marginRight: '10px' }} onClick={this.showShortPreview.bind(this)}>Alternate/Short Form</button>
+        <button className="btn btn-default pull-right" style={{ marginRight: '10px' }} onClick={this.showRoPreview.bind(this)}>Read Only Form</button>
+
+        { this.state.previewVisible &&
+          <div className={modalClass}>
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-body">
+                    <ReactFormGenerator
+                        download_path=""
+                        back_action="/"
+                        back_name="Back"
+                        answer_data={{}}
+                        action_name="Save"
+                        form_action="/"
+                        form_method="POST"
+                        onSubmit={this.submit}
+                        variables={this.props.variables}
+                        data={this.state.data} />
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.closePreview.bind(this)}>Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+
+        { this.state.roPreviewVisible &&
+          <div className={roModalClass}>
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-body">
+                    <ReactFormGenerator
+                        download_path=""
+                        back_action="/"
+                        back_name="Back"
+                        answer_data={{}}
+                        action_name="Save"
+                        form_action="/"
+                        form_method="POST"
+                        read_only={true}
+                        variables={this.props.variables}
+                        hide_actions={true} data={this.state.data} />
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.closePreview.bind(this)}>Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+
+        { this.state.shortPreviewVisible &&
+          <div className={shortModalClass}>
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-body">
+                    <ReactFormGenerator
+                        download_path=""
+                        back_action=""
+                        answer_data={{}}
+                        form_action="/"
+                        form_method="POST"
+                        data={this.state.data}
+                        display_short={true}
+                        variables={this.props.variables}
+                        hide_actions={false} />
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.closePreview.bind(this)}>Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
+}
