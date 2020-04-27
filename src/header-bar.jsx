2805913b972f5efed5df8e69e89cbeac8b11443e
Ryszard Trojnacki <ryszard.trojnacki@gmail.com>
2020-04-27 21:51:22 +0200

    Minor corrections in Font Awesome icons.

--- src/header-bar.jsx
+++ src/header-bar.jsx
@@ -11,9 +11,9 @@ export default class HeaderBar extends React.Component {
         <span className="label label-default">{this.props.data.text}</span>
         <div className="toolbar-header-buttons">
           { this.props.data.element !== 'LineBreak' &&
-            <div className="btn is-isolated btn-school" onClick={this.props.editModeOn.bind(this.props.parent, this.props.data)}><i className="is-isolated fas fa-pencil-square-o"></i></div>
+            <div className="btn is-isolated btn-school" onClick={this.props.editModeOn.bind(this.props.parent, this.props.data)}><i className="is-isolated fas fa-edit"></i></div>
           }
-          <div className="btn is-isolated btn-school" onClick={this.props.onDestroy.bind(this, this.props.data)}><i className="is-isolated fas fa-trash-o"></i></div>
+          <div className="btn is-isolated btn-school" onClick={this.props.onDestroy.bind(this, this.props.data)}><i className="is-isolated fas fa-trash"></i></div>
         </div>
       </div>
     );
