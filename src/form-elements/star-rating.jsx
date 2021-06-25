1b99e70f47e8ae83a224cc9b0fb64a978b535363
Kiho Chang <kihoc22@gmail.com>
2021-06-25 08:23:02 -0700

    Update node-sass, hide star-rating inputbox (#141)

--- src/form-elements/star-rating.jsx
+++ src/form-elements/star-rating.jsx
@@ -249,7 +249,7 @@ export default class StarRating extends React.Component {
       <span className="react-star-rating">
         <span ref={c => this.rootNode = c} style={{ cursor: 'pointer' }} className={classes}>
           {starRating}
-          <input type="number" name={this.props.name} value={this.state.ratingCache.rating} style={{ display: 'none !important' }} min={this.min} max={this.max} readOnly style={{
+          <input type="hidden" name={this.props.name} value={this.state.ratingCache.rating} style={{ display: 'none !important' }} min={this.min} max={this.max} readOnly style={{
             width: 65,
           }}/>
         </span>
