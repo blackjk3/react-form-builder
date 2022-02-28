e5c97ecc9ddaeb8564f6d8a7635f9f45da896711
Kiho <kihoc22@gmail.com>
2022-02-28 00:28:35 -0800

    Fix eslint issues

--- src/form-place-holder.jsx
+++ src/form-place-holder.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl } from "react-intl";
+import { injectIntl } from 'react-intl';
 
 const PLACE_HOLDER = 'form-place-holder';
 const PLACE_HOLDER_HIDDEN = 'form-place-holder-hidden';
 
 class PlaceHolder extends React.Component {
   render() {
-    const intl = this.props.intl;
+    const { intl } = this.props;
     const placeHolderClass = this.props.show ? PLACE_HOLDER : PLACE_HOLDER_HIDDEN;
-    const placeHolder = this.props.show ? 
-          (this.props.text == 'Dropzone' ? intl.formatMessage({ id : 'drop-zone' }) : this.props.text) : '';
-    
+    // eslint-disable-next-line no-nested-ternary
+    const placeHolder = this.props.show ?
+          (this.props.text === 'Dropzone' ? intl.formatMessage({ id: 'drop-zone' }) : this.props.text) : '';
+
     return (
       <div className={placeHolderClass} >
         <div>{placeHolder}</div>
