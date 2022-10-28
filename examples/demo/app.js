adc16670a4ee00de1231d869582e7f4dd4b4ea14
Kiho <kihoc22@gmail.com>
2022-10-27 23:49:29 -0700

    Fix Issue #232 - Elements Deleting when dragging

--- examples/demo/app.js
+++ examples/demo/app.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { ReactFormBuilder } from 'react-form-builder2';
 import DemoBar from './demobar';
 import * as variables from './variables';
 
 import 'react-form-builder2/dist/app.css';
 
-const app = ReactDOM.createRoot(
+ReactDOM.render(
+  <ReactFormBuilder variables={variables}
+    url='/api/formdata'
+    saveUrl='/api/formdata'
+  />,
   document.getElementById('form-builder'),
 );
-app.render(<ReactFormBuilder variables={variables}
-  url='/api/formdata'
-  saveUrl='/api/formdata'
-/>);
 
-const demo = ReactDOM.createRoot(
+ReactDOM.render(
+  <DemoBar variables={variables} />,
   document.getElementById('demo-bar'),
 );
-demo.render(<DemoBar variables={variables} />);
