a328e712b0cdb8e7b49903b7353cea41bc0ea0cc
Usama Zulfiqar <100605718+cashpricemd@users.noreply.github.com>
2023-05-19 12:20:17 +0500

    code clean up

--- app.js
+++ app.js
@@ -13,89 +13,89 @@ const saveUrl = '/api/formdata';
 
 const TestComponent = () => <h2>Hello</h2>;
 
-const MyInput = React.forwardRef((props, ref) => {
-  const { name, defaultValue, disabled } = props;
-  return (
-    <>
-      <label style={{ marginRight: '1rem' }}><b>{ props.data.label }</b></label>
-      <input ref={ref} name={name} defaultValue={defaultValue} disabled={disabled} />;
-    </>
-  );
-});
+// const MyInput = React.forwardRef((props, ref) => {
+//   const { name, defaultValue, disabled } = props;
+//   return (
+//     <>
+//       <label style={{ marginRight: '1rem' }}><b>{ props.data.label }</b></label>
+//       <input ref={ref} name={name} defaultValue={defaultValue} disabled={disabled} />;
+//     </>
+//   );
+// });
 
-Registry.register('MyInput', MyInput);
-Registry.register('TestComponent', TestComponent);
+// Registry.register('MyInput', MyInput);
+// Registry.register('TestComponent', TestComponent);
 
-const items = [{
-    key: 'Header',
-  }, {
-    key: 'TextInput',
-  }, {
-    key: 'TextArea',
-  }, {
-    key: 'RadioButtons',
-  }, {
-    key: 'Checkboxes',
-  }, {
-    key: 'Image',
-  },
-  {
-    key: 'FieldSet',
-    label:"Field Set",
-    name:"Field Set",
+// const items = [{
+//     key: 'Header',
+//   }, {
+//     key: 'TextInput',
+//   }, {
+//     key: 'TextArea',
+//   }, {
+//     key: 'RadioButtons',
+//   }, {
+//     key: 'Checkboxes',
+//   }, {
+//     key: 'Image',
+//   },
+//   {
+//     key: 'FieldSet',
+//     label:"Field Set",
+//     name:"Field Set",
     
-  },
-  {
-    group_name: 'Multi Column Row',
-    key: 'TwoColumnRow'
-  },
-  {
-    group_name: 'Multi Column Row',
-    key: 'ThreeColumnRow'
-  },
-  {
-    group_name: 'Multi Column Row',
-    key: 'FourColumnRow',
-    element: 'MultiColumnRow',
-  },
-  {
-    group_name: 'Multi Column Row',
-    key: 'FiveColumnRow',
-    element: 'MultiColumnRow',
-  },  
-  {
-    group_name: 'Multi Column Row',
-    key: 'SixColumnRow',
-    element: 'MultiColumnRow',
-  },
-  {
-    group_name: 'Custom Element',
-    key: 'TestComponent',
-    element: 'CustomElement',
-    component: TestComponent,
-    type: 'custom',
-    field_name: 'test_component',
-    name: 'Something You Want',
-    icon: 'fa fa-cog',
-    static: true,
-    props: { test: 'test_comp' },
-    label: 'Label Test',
-  },
-  {
-    group_name: 'Custom Element',
-    key: 'MyInput',
-    element: 'CustomElement',
-    component: MyInput,
-    type: 'custom',
-    forwardRef: true,
-    bare: true,
-    field_name: 'my_input_',
-    name: 'My Input',
-    icon: 'fa fa-cog',
-    props: { test: 'test_input' },
-    label: 'Label Input',
-  },
-];
+//   },
+//   {
+//     group_name: 'Multi Column Row',
+//     key: 'TwoColumnRow'
+//   },
+//   {
+//     group_name: 'Multi Column Row',
+//     key: 'ThreeColumnRow'
+//   },
+//   {
+//     group_name: 'Multi Column Row',
+//     key: 'FourColumnRow',
+//     element: 'MultiColumnRow',
+//   },
+//   {
+//     group_name: 'Multi Column Row',
+//     key: 'FiveColumnRow',
+//     element: 'MultiColumnRow',
+//   },  
+//   {
+//     group_name: 'Multi Column Row',
+//     key: 'SixColumnRow',
+//     element: 'MultiColumnRow',
+//   },
+//   {
+//     group_name: 'Custom Element',
+//     key: 'TestComponent',
+//     element: 'CustomElement',
+//     component: TestComponent,
+//     type: 'custom',
+//     field_name: 'test_component',
+//     name: 'Something You Want',
+//     icon: 'fa fa-cog',
+//     static: true,
+//     props: { test: 'test_comp' },
+//     label: 'Label Test',
+//   },
+//   {
+//     group_name: 'Custom Element',
+//     key: 'MyInput',
+//     element: 'CustomElement',
+//     component: MyInput,
+//     type: 'custom',
+//     forwardRef: true,
+//     bare: true,
+//     field_name: 'my_input_',
+//     name: 'My Input',
+//     icon: 'fa fa-cog',
+//     props: { test: 'test_input' },
+//     label: 'Label Input',
+//   },
+// ];
 
 
 const App = () => (
